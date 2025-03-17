07e338d79f02cb8757a3b631e477492b83283c56
Kazuki Kijima <kazukiyoshida96@gmail.com>
2025-03-17 12:32:14 -0700

    Add /health API endpoint

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+func healthCheck(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"status": "ok",
+	})
+}
+
 func main() {
-	fmt.Println("Hello, world!")
+	r := gin.Default()
+	r.GET("/health", healthCheck)
+	r.Run(":8000")
 }
